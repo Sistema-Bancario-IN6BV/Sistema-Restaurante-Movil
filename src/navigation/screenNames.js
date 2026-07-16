b5f882345bac03f89a85cae0432a7ca5f5a98821
erodriguez-2024250 <erodriguez-2024250@kinal.edu.gt>
2026-07-15 20:36:15 -0600

    feat: define navigation screen constant names.

---
+++ src/navigation/screenNames.js
@@ -0,0 +1,46 @@
+export const ROOT_SCREENS = {
+  MAIN_TABS: 'MainTabs',
+  PROFILE: 'Profile'
+};
+
+export const AUTH_SCREENS = {
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+  FORGOT_PASSWORD: 'ForgotPassword',
+  VERIFY_EMAIL: 'VerifyEmail'
+};
+
+export const TABS = {
+  HOME: 'HomeTab',
+  EVENTS: 'EventsTab',
+  ORDERS: 'OrdersTab',
+  RESERVATIONS: 'ReservationsTab'
+};
+
+export const HOME_STACK_SCREENS = {
+  HOME: 'Home',
+  FILTER: 'Filter',
+  RESTAURANT_DETAIL: 'RestaurantDetail',
+  MENU_ITEM_DETAIL: 'MenuItemDetail',
+  CART: 'Cart',
+  CREATE_RESERVATION: 'CreateReservation',
+  NOTIFICATIONS: 'Notifications'
+};
+
+export const EVENTS_STACK_SCREENS = {
+  LIST: 'EventsList',
+  DETAIL: 'EventDetail'
+};
+
+export const ORDERS_STACK_SCREENS = {
+  LIST: 'OrdersList',
+  DETAIL: 'OrderDetail',
+  TRACKING: 'OrderTracking',
+  RATE: 'RateOrder'
+};
+
+export const RESERVATIONS_STACK_SCREENS = {
+  LIST: 'ReservationsList',
+  CREATE: 'CreateReservation',
+  DETAIL: 'ReservationDetail'
+};
